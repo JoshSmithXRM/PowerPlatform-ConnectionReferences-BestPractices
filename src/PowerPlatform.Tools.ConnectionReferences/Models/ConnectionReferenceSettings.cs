9e75513cb8a1e66b84214646ee41c20ad0a3622e
Josh Smith <6895577+JoshSmithXRM@users.noreply.github.com>
2025-06-14 13:56:23 -0500

    update default

--- src/PowerPlatform.Tools.ConnectionReferences/Models/ConnectionReferenceSettings.cs
+++ src/PowerPlatform.Tools.ConnectionReferences/Models/ConnectionReferenceSettings.cs
@@ -2,6 +2,6 @@ namespace PowerPlatform.Tools.ConnectionReferences.Models;
 
 public class ConnectionReferenceSettings
 {
-    public string Prefix { get; set; } = "shared";
+    public string Prefix { get; set; } = "new";
     public Dictionary<string, ProviderMapping> ProviderMappings { get; set; } = new();
 }
